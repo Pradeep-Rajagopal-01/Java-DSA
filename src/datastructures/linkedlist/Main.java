a9a0484eb531b751f450344e988cfad835b7aac9
Pradthelegend <pradeep.studies06@gmail.com>
2025-08-21 00:13:25 -0400

    Partial Commit

---
+++ src/datastructures/linkedlist/Main.java
@@ -0,0 +1,9 @@
+package datastructures.linkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyLinkedList myLinkedList = new MyLinkedList(1);
+    }
+}
--- src/datastructures/linkedlist/MyLinkedList.java
+++ src/datastructures/linkedlist/MyLinkedList.java
@@ -2,5 +2,25 @@ package datastructures.linkedlist;
 
 public class MyLinkedList {
 
+    private Node head;
+    private Node tail;
+    private int length;
 
+    class Node{
+
+        int value;
+        Node next;
+
+        Node (int value){
+            this.value = value;
+        }
+    }
+
+    public MyLinkedList(int value){
+
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
 }
