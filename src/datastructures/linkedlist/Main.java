ad9fb1660fc5771298a7b97ba5080aff26fd9969
Pradthelegend <pradeep.studies06@gmail.com>
2025-08-22 22:57:53 -0400

    Partil commit

--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         MyLinkedList myLinkedList = new MyLinkedList(1);
+        myLinkedList.getHead();
+        myLinkedList.gettail();
+        myLinkedList.getLength();
+        myLinkedList.printList();
     }
 }
