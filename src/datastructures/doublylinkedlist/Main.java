7bc7ef90b336b1e70e2818cfae2851f08008510c
Pradthelegend <pradeep.studies06@gmail.com>
2025-09-22 23:33:04 -0400

    Added lots of code constructor,append,removeLast etc..

--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -1,4 +1,17 @@
 package datastructures.doublylinkedlist;
 
 public class Main {
+
+    public static void main(String[] args) {
+        MyDoublyLinkedList doublyLinkedList = new MyDoublyLinkedList(7);
+        doublyLinkedList.append(5);
+        doublyLinkedList.append(8);
+        doublyLinkedList.removeLast();
+
+        System.out.println(doublyLinkedList.getLength());
+
+        System.out.println("Printing all the items from the list : ");
+        doublyLinkedList.printList();
+
+    }
 }
--- src/datastructures/doublylinkedlist/MyDoublyLinkedList.java
+++ src/datastructures/doublylinkedlist/MyDoublyLinkedList.java
@@ -1,4 +1,77 @@
 package datastructures.doublylinkedlist;
 
+import datastructures.linkedlist.MyLinkedList;
+
 public class MyDoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{ // Everytime we create an object of this class a new node will be created.
+        int value;
+        Node next;
+        Node prev;
+
+        Node (int value){
+            this.value = value;
+        }
+    }
+    public MyDoublyLinkedList(int value){
+        Node newNode = new Node(value);
+        head=newNode;
+        tail=newNode;
+        length=1;
+    }
+
+    public void append(int value){
+        Node newNode = new Node(value);
+        if (head==null){
+            head=newNode;
+            tail=newNode;
+        }else {
+            tail.next=newNode;
+            newNode.prev=tail;
+            tail=newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) {
+            return null;
+        } else if (length==1) {
+            Node temp=head;
+            head=null;
+            tail=null;
+            length--;
+            return temp;
+        } else {
+            Node temp = tail;
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+            length--;
+            return temp;
+        }
+    }
+
+    public void printList(){
+        Node temp = head;
+        while (temp!=null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
 }
