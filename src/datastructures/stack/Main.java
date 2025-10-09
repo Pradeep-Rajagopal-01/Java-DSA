c7252d529fb192270c11aebc2a4cc3301ea02c59
Pradthelegend <pradeep.studies06@gmail.com>
2025-10-09 13:24:55 -0400

    Started working on Stack, I'm implimenting it using linked list.

---
+++ src/datastructures/stack/Main.java
@@ -0,0 +1,18 @@
+package datastructures.stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyStack myStack = new MyStack(7);
+
+
+
+//        myStack.getTop();
+
+//        myStack.getHeight();
+
+        System.out.println("Printing all the items in the stack : ");
+        myStack.printStack();
+    }
+}
