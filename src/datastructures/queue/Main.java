06bd360f7b7eda91b1d032701db27e0e7fb56ab2
Pradthelegend <pradeep.studies06@gmail.com>
2025-10-09 20:35:07 -0400

    Started working on queue and added methods to perform enqueue and dequeue

---
+++ src/datastructures/queue/Main.java
@@ -0,0 +1,17 @@
+package datastructures.queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue(7);
+        myQueue.enQueue(8);
+        myQueue.enQueue(9);
+
+        myQueue.deQueue();
+
+        System.out.println("Length of the Queue is : " + myQueue.getLength());
+
+        System.out.println("Printing all the items in the queue : ");
+        myQueue.printQueue();
+    }
+}
