126904ed38f2eb77236520e09e81a00299062c3c
Pradthelegend <pradeep.studies06@gmail.com>
2025-10-13 23:28:05 -0400

    Just moved the code from the Stack class to the Main class

--- src/datastructures/stack/implimentation/arrayliststack/Main.java
+++ src/datastructures/stack/implimentation/arrayliststack/Main.java
@@ -2,6 +2,45 @@ package datastructures.stack.implimentation.arrayliststack;
 
 public class Main {
 
+
+    //DSA Question-Reverse String ( ** Interview Question)
+    public static String reverseString(String string) {
+
+        Stack<Character> charStack = new Stack<>();
+
+        for (char c : string.toCharArray()) {
+            charStack.push(c);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!charStack.isEmpty()) {
+            stringBuilder.append(charStack.pop());
+        }
+
+        return stringBuilder.toString();
+    }
+
+    //DSA Question-Parentheses Balanced ( ** Interview Question)
+    public static boolean isBalancedParentheses(String string) {
+
+        Stack<Character> characterStack = new Stack<>();
+        for (Character c : string.toCharArray()) {
+            if (c == '[' || c == '{' || c == '(') {
+                characterStack.push(c);
+            } else if (c == ']' || c == '}' || c == ')') {
+                if (characterStack.isEmpty()) return false;
+                char checker = characterStack.pop();
+                if ((checker == '[' && c == ']') || (checker == '{' && c == '}') || (checker == '(' && c == ')')) {
+                    // matched pair, continue
+                } else {
+                    return false;
+                }
+            }
+        }
+        return characterStack.isEmpty();
+    }
+
+
     public static void main(String[] args) {
         Stack stack = new Stack();
 //        stack.push(7);
@@ -13,20 +52,15 @@ public class Main {
 
         stack.pop();
 
+//        System.out.println("Reversed String : " + reverseString("Prad")); //DSA Question-Reverse String ( ** Interview Question)
+
+//        System.out.println(isBalancedParentheses("[()"));//DSA Question-Parentheses Balanced ( ** Interview Question)
 
         System.out.println("Size of the stack is : " + stack.size());
 
         System.out.println("Printing all the items from the stack : ");
         stack.printStack();
 
-//        System.out.println("Reversed String : " + stack.reverseString("Prad"));
-
-        System.out.println(stack.isBalancedParentheses(")"));
-
-
     }
-
-
-
 
 }
--- src/datastructures/stack/implimentation/arrayliststack/Stack.java
+++ src/datastructures/stack/implimentation/arrayliststack/Stack.java
@@ -44,41 +44,4 @@ public class Stack<T> {
         return stackList.remove(size()-1);
     }
 
-    //DSA Question-Reverse String ( ** Interview Question)
-    public String reverseString(String string) {
-
-        Stack<Character> charStack = new Stack<>();
-
-        for (char c : string.toCharArray()) {
-            charStack.push(c);
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        while (!charStack.isEmpty()) {
-            stringBuilder.append(charStack.pop());
-        }
-
-        return stringBuilder.toString();
-    }
-
-    //DSA Question-Parentheses Balanced ( ** Interview Question)
-    public boolean isBalancedParentheses(String string){
-
-        Stack<Character> characterStack = new Stack<>();
-        for (Character c : string.toCharArray()){
-            if (c=='[' || c=='{' || c=='('){
-                characterStack.push(c);
-            } else if (c==']' || c=='}' || c==')') {
-                if (characterStack.isEmpty()) return false;
-                char checker=characterStack.pop();
-                if ((checker=='[' && c==']') || (checker=='{' && c=='}') || (checker=='(' && c==')')){
-                    // matched pair, continue
-                }else {
-                    return false;
-                }
-            }
-        }
-        return characterStack.isEmpty();
-    }
-
 }
