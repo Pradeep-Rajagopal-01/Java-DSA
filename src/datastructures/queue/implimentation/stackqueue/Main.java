cc78f4ee803e0f1d66ac5554380fbfae65c97ba4
Pradthelegend <pradeep.studies06@gmail.com>
2025-10-23 22:56:32 +0530

    Solved Queue Using Stacks: Enqueue (DSA Problem)

---
+++ src/datastructures/queue/implimentation/stackqueue/Main.java
@@ -0,0 +1,19 @@
+package datastructures.queue.implimentation.stackqueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StackQueue stackQueue = new StackQueue();
+
+
+        stackQueue.enqueue(7);
+        stackQueue.enqueue(8);
+        stackQueue.enqueue(9);
+
+
+        stackQueue.printStack();
+
+
+    }
+
+}
---
+++ src/datastructures/queue/implimentation/stackqueue/StackQueue.java
@@ -0,0 +1,40 @@
+package datastructures.queue.implimentation.stackqueue;
+
+import java.util.Stack;
+
+public class StackQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public StackQueue(){
+        stack1=new Stack<>();
+        stack2=new Stack<>();
+    }
+
+    public int peek() {
+        return stack1.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty();
+    }
+
+    public void printStack() {
+        for (int i = stack1.size()-1; i >= 0; i--) {
+            System.out.println(stack1.get(i));
+        }
+    }
+
+    //DSA Question-Queue Using Stacks: Enqueue ( ** Interview Question)
+    public void enqueue(int value){
+
+        while (!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        stack1.push(value);
+
+        while (!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+    }
+}
