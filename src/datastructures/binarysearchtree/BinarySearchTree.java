5745704bb8c964905179a56061c62c4e67f93ed3
Pradthelegend <pradeep.studies06@gmail.com>
2025-10-31 00:43:26 +0530

    Working on contains()(Partial commit)

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -40,4 +40,25 @@ public class BinarySearchTree {
             }
         }
     }
+
+    public boolean contains(int value){
+        if (root==null) return false;
+        Node temp=root;
+        while (temp!=null){
+            if (value<temp.value){
+                temp=temp.left;
+                if (temp.left!=null && value==temp.value){
+                    return true;
+                }
+            } else if (value>temp.value) {
+                temp=temp.right;
+                if (temp.right!=null && value==temp.value){
+                    return true;
+                }
+            }else {
+                return false;
+            }
+        }
+        return false;
+    }
 }
