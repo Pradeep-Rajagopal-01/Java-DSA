bbcb9329a12a65d2a04ed4c50704ee29ac5125e8
Pradthelegend <pradeep.studies06@gmail.com>
2025-11-03 23:10:46 +0530

    Added a printTable() to print all the items and also started working on creating the hash()

--- src/datastructures/hashtable/HashTable.java
+++ src/datastructures/hashtable/HashTable.java
@@ -16,6 +16,29 @@ public class HashTable {
     }
 
     public HashTable(){
-        dataMap=new Node[size];
+        dataMap=new Node[size];//Creates an array of the specified size.
+    }
+
+    private int hash(String key){
+        int hash=0;
+        char[] keyChars=key.toCharArray();
+
+        for (int i = 0; i <keyChars.length ; i++) {
+            int asciiValue=keyChars[i];
+        }
+        return 1;
+    }
+
+    public void printTable() {
+        for(int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            if(dataMap[i] != null) {
+                Node temp = dataMap[i];
+                while (temp != null) {
+                    System.out.println("   {" + temp.key + ", " + temp.value + "}");
+                    temp = temp.next;
+                }
+            }
+        }
     }
 }
--- src/datastructures/hashtable/Main.java
+++ src/datastructures/hashtable/Main.java
@@ -1,4 +1,11 @@
 package datastructures.hashtable;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        HashTable hashTable=new HashTable();
+
+        hashTable.printTable();
+    }
 }
