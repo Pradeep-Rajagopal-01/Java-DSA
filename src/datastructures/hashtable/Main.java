46fe88401b9beb4414c6f296a7666024452ccacb
Pradthelegend <pradeep.studies06@gmail.com>
2025-11-06 22:40:12 +0530

    Added itemInCommon() which is an interview question

--- src/datastructures/hashtable/Main.java
+++ src/datastructures/hashtable/Main.java
@@ -1,7 +1,22 @@
 package datastructures.hashtable;
 
+import java.util.HashMap;
+
 public class Main {
 
+
+    public static boolean itemInCommon(int array1[], int array2[]){
+        HashMap<Integer,Boolean> hashMap = new HashMap<Integer, Boolean>();
+
+        for (int i : array1){
+            hashMap.put(i,true);
+        }
+
+        for (int j : array2){
+            if (hashMap.get(j) !=null) return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
 
         HashTable hashTable=new HashTable();
@@ -14,8 +29,13 @@ public class Main {
 
 //        System.out.println("Value of the provided key: "  + hashTable.get("BMW"));
 
-        System.out.println(hashTable.keys());
+//        System.out.println(hashTable.keys());
 
-//        hashTable.printTable();
+        //        hashTable.printTable();
+
+        int[] array1 = {1,4,7};
+        int[] array2 = {2,7,9};
+
+        System.out.println(itemInCommon(array1,array2)); //Interview Question.
     }
 }
