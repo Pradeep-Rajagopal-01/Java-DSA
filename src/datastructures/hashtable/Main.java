a06ff377f76e1468c2b911f4474c2f3dc0f5edf9
Pradthelegend <pradeep.studies06@gmail.com>
2025-11-07 21:02:28 +0530

    Working on findDuplicates() DSA Problem (Partial commit)

--- src/datastructures/hashtable/Main.java
+++ src/datastructures/hashtable/Main.java
@@ -1,11 +1,11 @@
 package datastructures.hashtable;
 
-import java.util.HashMap;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
 
-
-    public static boolean itemInCommon(int array1[], int array2[]){
+    public static boolean itemInCommon(int array1[], int array2[]){//Interview Question.
         HashMap<Integer,Boolean> hashMap = new HashMap<Integer, Boolean>();
 
         for (int i : array1){
@@ -17,6 +17,29 @@ public class Main {
         }
         return false;
     }
+
+    public static List<Integer> findDuplicates(int[] nums){
+        HashMap<Integer,Integer> numCounts = new HashMap<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int num : nums) {
+            if (numCounts.get(num) == null) {
+                numCounts.put(num, 0);
+            }
+            numCounts.replace(num,numCounts.get(num)+1);
+        }
+        System.out.println(numCounts);
+        ArrayList<Integer> duplicates = new ArrayList<>();
+        for (int i = 0; i <nums.length ; i++) {
+            if (numCounts.get(i) >1){
+                duplicates.add(1);
+            }
+        }
+        return duplicates;
+    }
+
+    
+
+
     public static void main(String[] args) {
 
         HashTable hashTable=new HashTable();
@@ -33,9 +56,14 @@ public class Main {
 
         //        hashTable.printTable();
 
-        int[] array1 = {1,4,7};
-        int[] array2 = {2,7,9};
+//        int[] array1 = {1,4,7};
+//        int[] array2 = {2,7,9};
+//        System.out.println(itemInCommon(array1,array2)); //Interview Question.
 
-        System.out.println(itemInCommon(array1,array2)); //Interview Question.
+
+        System.out.println(findDuplicates(new int[] {5,5,5,2,2}));
+
+
+
     }
 }
