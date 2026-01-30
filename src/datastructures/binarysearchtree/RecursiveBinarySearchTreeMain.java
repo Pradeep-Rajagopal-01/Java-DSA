affc94d867d11b874b15483e2d51a18ef6619e07
Pradthelegend <pradeep.studies06@gmail.com>
2026-01-30 19:46:06 +0530

    This is the 'Main()' class for RecursiveBinarySearchTree

--- src/datastructures/binarysearchtree/RecursiveBinarySearchTreeMain.java
+++ src/datastructures/binarysearchtree/RecursiveBinarySearchTreeMain.java
@@ -1,4 +1,22 @@
 package datastructures.binarysearchtree;
 
 public class RecursiveBinarySearchTreeMain {
+
+    public static void main(String[] args) {
+
+        //This is a recursive version of BST (Methods use recursion to move to the next nodes).
+
+        RecursiveBinarySearchTree rBST = new RecursiveBinarySearchTree();
+
+        rBST.recursiveInsert(5);
+        rBST.recursiveInsert(7);
+        rBST.recursiveInsert(8);
+        rBST.recursiveInsert(3);
+        rBST.recursiveInsert(1);
+
+        System.out.println(rBST.recursiveContains(5));
+        rBST.deleteNode(5);
+
+
+    }
 }
