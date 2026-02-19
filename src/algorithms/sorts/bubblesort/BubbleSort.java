3c1619d0e1ad7b90e84872ed22d0bb18f8245adf
Pradthelegend <pradeep.studies06@gmail.com>
2026-02-19 22:10:27 +0530

    Started working on algorithms so created a spearate package for it

---
+++ src/algorithms/sorts/bubblesort/BubbleSort.java
@@ -0,0 +1,14 @@
+package algorithms.sorts.bubblesort;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array){
+        for (int i = array.length-1; i > 0 ; i--) {
+            for (int j = 0; j <array.length ; j++) {
+                if (array[j] > array[j+1]){
+
+                }
+            }
+        }
+    }
+}
