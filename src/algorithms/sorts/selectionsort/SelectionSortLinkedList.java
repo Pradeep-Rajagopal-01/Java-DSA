f7c2cd243e3c8015bf9024232201e84ce47fd448
Pradthelegend <pradeep.studies06@gmail.com>
2026-03-03 15:09:14 +0530

    Solved Selection sort of linked list DSA probelm

--- src/algorithms/sorts/selectionsort/SelectionSortLinkedList.java
+++ src/algorithms/sorts/selectionsort/SelectionSortLinkedList.java
@@ -60,7 +60,44 @@ public class SelectionSortLinkedList {
         length++;
     }
 
+    //DSA Question-Selection Sort of LL ( ** Interview Question)
+    public void selectionSort(SelectionSortLinkedList list){
+        if (list.getLength() < 2) return;
 
+        Node current=list.head;
+        while (current.next!=null){
+            Node minNode=current;
+            Node traverse=minNode.next;
+            while (traverse!=null){
+                if (traverse.value < minNode.value){
+                    minNode=traverse;
+                }
+                traverse=traverse.next;
+            }
+            if (current!= minNode) {
+                int temp= minNode.value;
+                minNode.value = current.value;
+                current.value = temp;
+            }
+            current = current.next;
+        }
+    }
 
+    public static void main(String[] args) {
+
+        SelectionSortLinkedList selectionSortLinkedList = new SelectionSortLinkedList(7);
+        selectionSortLinkedList.append(9);
+        selectionSortLinkedList.append(4);
+        selectionSortLinkedList.append(6);
+        selectionSortLinkedList.append(2);
+        selectionSortLinkedList.append(1);
+        selectionSortLinkedList.append(3);
+
+        selectionSortLinkedList.selectionSort(selectionSortLinkedList);
+
+        selectionSortLinkedList.printList();
+    }
 }
+
+
 
