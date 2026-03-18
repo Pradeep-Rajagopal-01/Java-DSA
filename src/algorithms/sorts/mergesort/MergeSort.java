8a35273048a681d74cf7808fdd14529587131135
Pradthelegend <pradeep.studies06@gmail.com>
2026-03-18 21:03:02 +0530

    Finished writing the merge sort algorithm

--- src/algorithms/sorts/mergesort/MergeSort.java
+++ src/algorithms/sorts/mergesort/MergeSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MergeSort {
 
-    //Note: MergeSort always takes in sorted arrays as input.
+    //Note: Merge always takes in sorted arrays as input.
     public static int[] merge(int[] array1, int[] array2){
         int[] combinedArray = new int[array1.length + array2.length];
         int index=0;
@@ -36,10 +36,24 @@ public class MergeSort {
         return combinedArray;
     }
 
-    public static void main(String[] args) {
-        int[] array1 = {1,3,7,8};
-        int[] array2 = {2,4,5,6};
+    public static int[] mergeSort(int[] array){
+        if (array.length == 1) return array;
 
-        System.out.println(Arrays.toString(merge(array1,array2)));
+        int minIndex = array.length/2;
+        int[] left = mergeSort(Arrays.copyOfRange(array,0,minIndex));
+        int[] right = mergeSort(Arrays.copyOfRange(array,minIndex,array.length));
+
+        return merge(left,right);
+    }
+
+    public static void main(String[] args) {
+//        int[] array1 = {1,3,7,8};
+//        int[] array2 = {2,4,5,6};
+//
+//        System.out.println(Arrays.toString(merge(array1,array2)));
+
+
+        int[] array = {1,4,6,7,3,9,2};
+        System.out.println(Arrays.toString(mergeSort(array)));
     }
 }
